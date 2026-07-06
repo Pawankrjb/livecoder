ab90644b8c37e70bd221030086fecca8a5559520
Pawan <sunnykishan@Pawan.local>
2026-07-06 06:05:58 +0530

    Day 1: React and Socket.IO connection

---
+++ clint/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+function App() {
+
+  useEffect(() => {
+
+    const socket = io("http://localhost:3000");
+
+    socket.on("connect", () => {
+      console.log("Connected :", socket.id);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+
+  }, []);
+
+  return (
+    <h1>Collaborative Editor</h1>
+  );
+}
+
+export default App;
