e2ce859165cf9c2cd155a1861e9e2ce7b5737095
Pawan <pawankrjb@gmail.com>
2026-07-10 05:42:49 +0530

    optmise code in test.js and trandform

--- shared/test.js
+++ shared/test.js
@@ -6,3 +6,24 @@ import { applyOperation } from "./engine.js";
 const newDocument = applyOperation(document, operation);
 
 console.log(newDocument);
+const document = {
+
+    content: "HelloA",
+
+    version: 1
+
+};
+const history={
+    type: "insert",
+        position: 5,
+        text: "A",
+        version: 0
+};
+const operation = {
+    type: "insert",
+    position: 5,
+    text: "B",
+    version: 0
+};
+
+
--- shared/transform.js
+++ shared/transform.js
@@ -1,9 +1,33 @@
 const transform = (incomingOp, existingOp) => {
 
-    if(incomingOp.position>= existingOp.position){
-        incomingOp.position++;
+    if(incomingOp.type==="insert"&& existingOp.type==="insert"){
+        if(incomingOp.position>=existingOp.position){
+            incomingOp.position++;
+        }
     }
+    if(incomingOp.type==="insert"&& existingOp.type==="delete"){
+        if(incomingOp.position>=existingOp.position){
+            incomingOp.position--;
+        }
+    }
+     if(incomingOp.type==="delete"&& existingOp.type==="insert"){
+        if(incomingOp.position>=existingOp.position){
+           incomingOp.position++;
+        }
+    }
+    if(incomingOp.type==="delete"&& existingOp.type==="delete"){
+        if(incomingOp.position>existingOp.position){
+           incomingOp.position--;
+        }
+         if(incomingOp.position==existingOp.position){
+           return null;
+        }
+    }
+    return incomingOp;
 };
+
+
+
 
 export {
 
